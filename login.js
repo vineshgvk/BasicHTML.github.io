9fbd8e570ad4100dce6f87e5d1d1f5d8ced310a9
vineshgvk <67590588+vineshgvk@users.noreply.github.com>
2021-07-01 16:54:58 +0530

    Add files via upload

---
+++ login.js
@@ -0,0 +1,19 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+if((username=="vinesh.gandhe@gmail.com" || username=="gvk7663@gmail.com")&& password=="user" )
+{
+    alert("login succesfully");
+    window.open("/HTML/home.html");
+
+    return false;
+
+}
+else
+{
+    alert("login failed");
+}
+
+
+}
